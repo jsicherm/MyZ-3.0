0c546d7776a8b5fb64ddface1aaed11175b6532d
TeeePeee <jordan.sicherman@brentwood.bc.ca>
2014-04-06 14:59:27 -0700

    Derped herps. Wrong version name on JAR and forgot some compat stuff.

--- src/myz/nmscode/compat/MessageUtils.java
+++ src/myz/nmscode/compat/MessageUtils.java
@@ -24,15 +24,23 @@ public class MessageUtils {
 	}
 
 	public static void holographic(String message, Player player, Location location, Player[] array) {
-		Hologram hologram = new Hologram(message);
-		hologram.setFollow(player);
-		hologram.show(location, array);
-		hologram.follow();
+		switch (MyZ.version) {
+		case v1_7_R1:
+			Hologram hologram = new Hologram(message);
+			hologram.setFollow(player);
+			hologram.show(location, array);
+			hologram.follow();
+			break;
+		}
 	}
 
 	public static void holographicDisplay(Location location, Player playerFor, String... msg) {
-		Hologram hologram = new Hologram(msg);
-		hologram.show(location, playerFor);
+		switch (MyZ.version) {
+		case v1_7_R1:
+			Hologram hologram = new Hologram(msg);
+			hologram.show(location, playerFor);
+			break;
+		}
 	}
 
 	public static double getHologramDistance() {
